839f15c2419d5cf6cae919004b09479777b283a9
Abel Chee <abel.chee@werkzgroup.com>
2019-09-02 21:10:40 +1000

    dep

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
-import TextareaMarkdownEditor from '../index';
+import { Textarea, TextareaMarkdownEditor } from '../index';
 
 describe('TextareaMarkdownEditor', () => {
   test('TextareaMarkdownEditor should initialize as it is', () => {
-    const component = renderer.create(<TextareaMarkdownEditor id="123" />);
+    const component = renderer.create(
+      <TextareaMarkdownEditor id="123">
+        <Textarea />
+      </TextareaMarkdownEditor>,
+    );
     const testInstance = component.root;
     const { props } = testInstance.findByType(TextareaMarkdownEditor);
     expect(props.id).toBe('123');
