658d6629ddcb9e552e1a03556adb5a2b6235f3c0
Abel Chee <abel.chee@gmail.com>
2019-09-08 19:28:33 +1000

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { TextareaMarkdownEditor } from 'react-textarea-markdown-editor';
 
-function App() {
+function App () {
+  const [lang, setLang] = useState('en');
   return (
     <div className="App">
-        <TextareaMarkdownEditor language="zh"  id="123" rows={10}/>
+      <button type="button" onClick={()=>setLang('en')}>English</button>
+      <button type="button" onClick={()=>setLang('zh')}>简体中文</button>
+      <br/>
+      <br/>
+      <TextareaMarkdownEditor language={lang} id="111111" rows={10}/>
     </div>
   );
 }
