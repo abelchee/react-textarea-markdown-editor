69c7dc3f01a2568c630397985664dc3c61994da9
Abel Chee <abel.chee@gmail.com>
2019-09-16 19:51:00 +1000

    convert to class

--- src/TextareaMarkdownEditor.tsx
+++ src/TextareaMarkdownEditor.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import * as React from 'react';
-import { useRef, useState } from 'react';
+import { RefObject } from 'react';
 import EnhancedTextarea from 'react-enhanced-textarea';
 import EditContext from './EditorContext';
 import EditorMenu from './EditorMenu';
@@ -25,95 +25,112 @@ export interface ITextareaMarkdownEditor {
   markers?: IMarkerGroup[];
 }
 
-const TextareaMarkdownEditor: React.FunctionComponent<ITextareaMarkdownEditor> = props => {
-  const { readOnly = false } = props;
-  const textareaRef = useRef<EnhancedTextarea>(null);
-  const [lineMarkers, setLineMarkers] = useState<string[]>([]);
-  const [edit, setEdit] = useState(!readOnly);
-  const [value, setValue] = useState(props.defaultValue);
+interface ITextareaMarkdownEditorState {
+  edit: boolean;
+  lineMarkers: string[];
+  value?: string;
+}
 
-  function toggleEdit() {
-    setEdit(!edit);
+class TextareaMarkdownEditor extends React.Component<ITextareaMarkdownEditor, ITextareaMarkdownEditorState> {
+  public static defaultProps = {
+    language: 'en',
+    readOnly: false,
+    rows: 5,
+  };
+
+  private textareaRef: RefObject<EnhancedTextarea>;
+
+  constructor(props: ITextareaMarkdownEditor) {
+    super(props);
+    this.textareaRef = React.createRef();
+    this.state = {
+      edit: !props.readOnly,
+      lineMarkers: [],
+      value: props.defaultValue,
+    };
+    this.toggleEdit = this.toggleEdit.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
-  function onChange(textarea: HTMLTextAreaElement) {
-    setValue(textarea.value);
-    if (props.onChange) {
-      props.onChange(textarea);
+  public render() {
+    const { readOnly = false } = this.props;
+    return (
+      <div id={this.props.id} className={classNames('tme-container', this.props.className)} style={this.props.style}>
+        <EditContext.Provider
+          value={{
+            focus: () => {
+              this.textareaRef.current!.focus();
+            },
+            mark: (prefix: string, suffix: string, defaultText: string, multipleLine: boolean) => {
+              if (multipleLine) {
+                this.textareaRef.current!.toggleMultipleLineMarker({ prefix, suffix, defaultText });
+              } else {
+                this.textareaRef.current!.toggleMarker({ prefix, suffix, defaultText });
+              }
+            },
+            markLine: (marker: string) => {
+              this.textareaRef.current!.toggleLineMarker(marker);
+            },
+            registerLineMarker: (marker: string) => {
+              const index = this.state.lineMarkers.indexOf(marker);
+              if (index < 0) {
+                this.setState({ ...this.state, lineMarkers: [...this.state.lineMarkers, marker] });
+              }
+            },
+            template: (template: string, multipleLine: boolean) => {
+              if (multipleLine) {
+                this.textareaRef.current!.toggleMultipleLineTemplate(template);
+              } else {
+                this.textareaRef.current!.toggleTemplate(template);
+              }
+            },
+          }}
+        >
+          <EditorMenu
+            markers={this.props.markers}
+            readOnly={readOnly}
+            language={this.props.language!}
+            isEditing={this.state.edit}
+            toggleEdit={this.toggleEdit}
+          />
+          {this.state.edit ? (
+            <EnhancedTextarea
+              id={this.props.textareaId}
+              className="tme-textarea"
+              ref={this.textareaRef}
+              rows={this.props.rows}
+              style={this.props.textareaStyle}
+              autoFocus={this.props.autoFocus}
+              defaultValue={this.state.value}
+              value={this.props.value}
+              onChange={this.onChange}
+              onKeyDown={this.props.onKeyDown}
+              onKeyPress={this.props.onKeyPress}
+              lineMarkers={this.state.lineMarkers}
+            />
+          ) : (
+            <div
+              className="tme-viewer"
+              dangerouslySetInnerHTML={{
+                __html: this.textareaRef.current ? this.props.doParse(this.textareaRef.current.value) : '',
+              }}
+            />
+          )}
+        </EditContext.Provider>
+      </div>
+    );
+  }
+
+  private toggleEdit() {
+    this.setState({ ...this.state, edit: !this.state.edit });
+  }
+
+  private onChange(textarea: HTMLTextAreaElement) {
+    this.setState({ ...this.state, value: textarea.value });
+    if (this.props.onChange) {
+      this.props.onChange(textarea);
     }
   }
-
-  return (
-    <div id={props.id} className={classNames('tme-container', props.className)} style={props.style}>
-      <EditContext.Provider
-        value={{
-          focus: () => {
-            textareaRef.current!.focus();
-          },
-          mark: (prefix: string, suffix: string, defaultText: string, multipleLine: boolean) => {
-            if (multipleLine) {
-              textareaRef.current!.toggleMultipleLineMarker({ prefix, suffix, defaultText });
-            } else {
-              textareaRef.current!.toggleMarker({ prefix, suffix, defaultText });
-            }
-          },
-          markLine: (marker: string) => {
-            textareaRef.current!.toggleLineMarker(marker);
-          },
-          registerLineMarker: (marker: string) => {
-            const index = lineMarkers.indexOf(marker);
-            if (index < 0) {
-              setLineMarkers([...lineMarkers, marker]);
-            }
-          },
-          template: (template: string, multipleLine: boolean) => {
-            if (multipleLine) {
-              textareaRef.current!.toggleMultipleLineTemplate(template);
-            } else {
-              textareaRef.current!.toggleTemplate(template);
-            }
-          },
-        }}
-      >
-        <EditorMenu
-          markers={props.markers}
-          readOnly={readOnly}
-          language={props.language!}
-          isEditing={edit}
-          toggleEdit={toggleEdit}
-        />
-        {edit ? (
-          <EnhancedTextarea
-            id={props.textareaId}
-            className="tme-textarea"
-            ref={textareaRef}
-            rows={props.rows}
-            style={props.textareaStyle}
-            autoFocus={props.autoFocus}
-            defaultValue={value}
-            value={props.value}
-            onChange={onChange}
-            onKeyDown={props.onKeyDown}
-            onKeyPress={props.onKeyPress}
-            lineMarkers={lineMarkers}
-          />
-        ) : (
-          <div
-            className="tme-viewer"
-            dangerouslySetInnerHTML={{
-              __html: textareaRef.current ? props.doParse(textareaRef.current.value) : '',
-            }}
-          />
-        )}
-      </EditContext.Provider>
-    </div>
-  );
-};
-
-TextareaMarkdownEditor.defaultProps = {
-  language: 'en',
-  readOnly: false,
-  rows: 5,
-};
+}
 
 export default TextareaMarkdownEditor;
